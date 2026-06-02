16714679f549d1cc52cf97f81c8e5283c5168d95
sudeephiremath-hash <sudeep.hiremath@ritgoa.ac.in>
2026-06-02 16:25:26 +0530

    updated this code

--- test.php
+++ test.php
@@ -2,10 +2,11 @@
 $servername = "localhost"; 
 $username = "root"; // default username for XAMPP MySQL 
 $password = ""; // default password for XAMPP MySQL is empty 
-$database = "dummy_db2"; // your database name
+$database = ""; // your database name
  // Create connection
  $conn = new mysqli($servername, $username, $password, $database);
 // Check connection 
 if ($conn->connect_error) 
 { die("Connection failed: " . $conn->connect_error); }
  echo "Connected successfully"; ?>
+aihfoanaf
